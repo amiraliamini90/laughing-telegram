e0eddd4a09ad4e9127f7e0ef786a9127d1f1b3fc
amiraliamini90 <aminiamirali653@gmail.com>
2024-11-28 01:11:52 +0330

    Add files via upload

---
+++ تمرین اول پایتون.py
@@ -0,0 +1,44 @@
+print('hello world')
+
+print('*****excercise1*****')
+
+print("********")
+print("**....**")
+print("**....**")
+print("********")
+
+
+print('*****excercise2*****')
+
+inches = float(input("meghdar inch ra vared namaeed ..."))
+harsantimete=(inches * 2.54)
+print('harsantimetr : ' , harsantimete)
+
+
+print('*****excercise3*****')
+
+x = float(input('shoa vared shavad ...'))
+c = float(input('ertefa vared shavad ...'))
+
+hajm = x*x*c*3.14
+print('hajm : ' , hajm)
+
+masahat = 2*3.14*x*x*c+3.14*x
+print('masahat : ' , masahat)
+
+
+print('*****excercise4*****')
+
+age = int(input('sen khod ra benevisid ...'))
+days = (age*3.156*10**7)
+sanieh = (days*24*60*60)
+print('sen shoma be sanieh :' , days and sanieh)
+
+
+print('*****excercise5*****')
+
+neveshteh = input('neveshteh mored nazar...')
+tedad = int(input('tedad tekrar matn mored nazar ...'))
+
+pasokh = neveshteh*tedad
+print(pasokh)
